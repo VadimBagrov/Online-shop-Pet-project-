171f488dd0593812b91f2c8e4a18dc9e3b325e32
Vadim <110101692+VadimBagrov@users.noreply.github.com>
2022-10-11 21:33:52 +0300

    11.10.2022 New ideas and design
    New ideas and design

---
+++ Online-store/src/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav-menu'),
+    menuItem = document.querySelectorAll('.nav-menu__list-item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav-menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav-menu_active');
+        })
+    })
+})
